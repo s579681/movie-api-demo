c182a8d78969c7cf28def98d6d125b78547ee10a
Stian Hauge <stian@MacBook-Pro.local>
2024-06-29 14:36:20 +0200

    Initial commit

---
+++ src/main/java/javademo/controller/MovieController.java
@@ -0,0 +1,56 @@
+package javademo.controller;
+
+import javademo.modal.Movie;
+import javademo.request.FilterRequest;
+import javademo.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class MovieController {
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @RequestMapping(value="/movies", method=RequestMethod.GET)
+    public List<Movie> getAllMovies() {
+        return movieService.getMovies();
+    }
+
+    //TODO Correctly handle errors
+    @RequestMapping(value="/movies/{id}", method=RequestMethod.GET)
+    public Optional<Movie> getMovieById(@PathVariable long id) {
+        Optional<Movie> movie = movieService.findMovieById(id);
+        if (movie.isPresent()) {
+            return movieService.findMovieById(id);
+        }
+        return movie;
+    }
+
+    @RequestMapping(value="/movies/search/title", method=RequestMethod.GET)
+    public List<Movie> searchByTitle(@RequestParam(value="title") String subString) {
+        return movieService.filterByTitle(subString);
+    }
+
+    @RequestMapping(value="/movies/search/actor", method=RequestMethod.GET)
+    public List<Movie> searchByActor(@RequestParam(value="actor") String actor) {
+        return movieService.filterByActor(actor);
+    }
+
+    @RequestMapping(value="/movies/search/director", method=RequestMethod.GET)
+    public List<Movie> searchByDirector(@RequestParam(value="director") String director) {
+        return movieService.filterByDirector(director);
+    }
+
+    @RequestMapping(value="/movies/filter", method=RequestMethod.POST)
+    public List<Movie> filterMovies(@RequestBody FilterRequest request) {
+        return movieService.filterMoviesByCriteria(request);
+    }
+}
