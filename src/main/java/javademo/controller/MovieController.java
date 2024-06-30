b5106bc0c9dd9470e8f02f83c1f45fbd324d46fe
Stian Hauge <shauge2206@gmail.com>
2024-07-01 00:36:38 +0200

    added swagger

--- src/main/java/javademo/controller/MovieController.java
+++ src/main/java/javademo/controller/MovieController.java
@@ -1,5 +1,9 @@
 package javademo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.Optional;
 import javademo.modal.Movie;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Movies", description = "Controller for retrieval of Movie entities based on specified criteria.")
 @RestController
 @RequestMapping("/api")
 public class MovieController {
@@ -24,12 +29,17 @@ public class MovieController {
     this.movieService = movieService;
   }
 
+  @Operation(summary = "Fetch all movies", description = "fetches all movie entities in database")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "successful operation")
+  })
   @RequestMapping(value = "/movies", method = RequestMethod.GET)
   public List<Movie> getAllMovies() {
     return movieService.getMovies();
   }
 
   //TODO Correctly handle errors
+  @Operation(summary = "Fetch movie based on ID", description = "fetches a movie entity using ID")
   @RequestMapping(value = "/movies/{id}", method = RequestMethod.GET)
   public Optional<Movie> getMovieById(@PathVariable long id) {
     Optional<Movie> movie = movieService.findMovieById(id);
@@ -39,21 +49,25 @@ public class MovieController {
     return movie;
   }
 
+  @Operation(summary = "Fetch movie entities based on title", description = "fetches all movie entities where title matches input string")
   @RequestMapping(value = "/movies/search/title", method = RequestMethod.GET)
   public List<Movie> searchByTitle(@RequestParam(value = "title") String subString) {
     return movieService.filterByTitle(subString);
   }
 
+  @Operation(summary = "Fetch movie entities based on actor", description = "fetches all movie entities where at least one actor matches input string")
   @RequestMapping(value = "/movies/search/actor", method = RequestMethod.GET)
   public List<Movie> searchByActor(@RequestParam(value = "actor") String actor) {
     return movieService.filterByActor(actor);
   }
 
+  @Operation(summary = "Fetch movie entities based on director", description = "fetches all movie entities where director matches input string")
   @RequestMapping(value = "/movies/search/director", method = RequestMethod.GET)
   public List<Movie> searchByDirector(@RequestParam(value = "director") String director) {
     return movieService.filterByDirector(director);
   }
 
+  @Operation(summary = "Fetch movie entities based on multiple criteria", description = "fetches all movie entities where genre,gross income, released year, runtime and IMDB rating are equal or greater than input values. Ignores criteria if it is not present.")
   @RequestMapping(value = "/movies/filter", method = RequestMethod.POST)
   public List<Movie> filterMovies(@RequestBody FilterRequest request) {
     return movieService.filterMoviesByCriteria(request);
